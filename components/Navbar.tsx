a29dc2f08e8033b19f84a6bcdf5ca9aa9b420d15
Muhammad Tanveer <muhammadtanveer.bcsf18@iba-suk.edu.pk>
2022-07-25 18:24:17 +0500

    userstore

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import {IoMdAdd} from 'react-icons/io'
+import { AiOutlineLogout } from 'react-icons/ai'
 import { GoogleLogin,googleLogout } from '@react-oauth/google'
 
 import Logo from '../utils/logo.png'
+import { createOrGetUser } from '../utils'
+import useAuthStore from '../store/authStore'
 
 const Navbar = () => {
-  const user = false;
+  const {userProfile, addUser, removeUser} = useAuthStore();
 
   return (
     <div className='w-full flex justify-between items-center
@@ -25,12 +29,42 @@ const Navbar = () => {
           SEARCH
         </div>
         <div>
-          {user ? (<div>
-            <div>Logged In</div>
+          {userProfile ? (<div>
+            <div className='flex gap-5 md:gap-10'>
+              <Link href="/upload">
+                <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+                  <IoMdAdd className='text-xl' /> {' '}
+                  <span className=' hidden md:block'>Upload</span>
+                </button>
+              </Link>
+              {userProfile.image &&(
+                <Link href="/">
+                <>
+                  <Image 
+                    width={40}
+                    height={40}
+                    className="rounded-full cursor-pointer"
+                    src={userProfile.image}
+                    alt="profile photo"
+                  />
+                </>
+              </Link>
+              )}
+              <button
+                type='button'
+                className='px-2'
+                onClick={() => {
+                  googleLogout();
+                  removeUser();
+                }}
+              >
+                <AiOutlineLogout color="red" fontSize={21} />
+              </button>
+            </div>
           </div>
           ): (
             <GoogleLogin 
-              onSuccess={(res) => console.log(res)}
+              onSuccess={(res) => createOrGetUser(res, addUser)}
               onError={() => console.log("Error")}/>
           )}
         </div>
